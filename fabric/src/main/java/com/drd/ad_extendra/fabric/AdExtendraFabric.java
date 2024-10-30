b6df05bacf106d6f2d835218c6763e3739e1557f
Dandi2k8 <drd.construction30@gmail.com>
2024-10-30 15:13:06 -0700

    Finishing version 1.1.1

--- fabric/src/main/java/com/drd/ad_extendra/fabric/AdExtendraFabric.java
+++ fabric/src/main/java/com/drd/ad_extendra/fabric/AdExtendraFabric.java
@@ -3,7 +3,6 @@ package com.drd.ad_extendra.fabric;
 import com.drd.ad_extendra.common.AdExtendra;
 import com.drd.ad_extendra.common.registry.ModBlocks;
 import com.drd.ad_extendra.common.registry.ModEntityTypes;
-import com.drd.ad_extendra.common.utils.fabric.ExistingStrippableLogs;
 import com.drd.ad_extendra.common.utils.fabric.ModLootModifiers;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
@@ -12,10 +11,10 @@ import net.fabricmc.fabric.api.registry.StrippableBlockRegistry;
 public class AdExtendraFabric implements ModInitializer {
     @Override
     public void onInitialize() {
+        StrippableBlockRegistry.register(earth.terrarium.adastra.common.registry.ModBlocks.GLACIAN_LOG.get(), earth.terrarium.adastra.common.registry.ModBlocks.STRIPPED_GLACIAN_LOG.get());
         AdExtendra.init();
         ModEntityTypes.registerAttributes((type, builder) -> FabricDefaultAttributeRegistry.register(type.get(), builder.get()));
 
-        ExistingStrippableLogs.register();
         StrippableBlockRegistry.register(ModBlocks.GLACIAN_WOOD.get(), ModBlocks.STRIPPED_GLACIAN_WOOD.get());
 
         ModLootModifiers.modifyLootTables();
